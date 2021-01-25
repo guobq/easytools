dc575ee02c6f2470cfab93e72188defb15337d8e
guobq <2278220079@qq.com>
2021-01-25 10:41:10 +0800

    path

--- MergePDF/mergepdf.py
+++ MergePDF/mergepdf.py
@@ -50,11 +50,7 @@ def MergePDF(dir_path, file_name):
 def cur_file_dir():
     # 获取脚本路径
     path = os.path.dirname(os.path.realpath(sys.argv[0]))
-    # 判断为脚本文件还是py2exe编译后的文件，如果是脚本文件，则返回的是脚本的目录，如果是py2exe编译后的文件，则返回的是编译后的文件路径
-    if os.path.isdir(path):
-        return path
-    elif os.path.isfile(path):
-        return os.path.dirname(path)
+    return path
 
 
 if __name__ == '__main__':
